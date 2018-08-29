5db6470be9a26543eeee5d38439b40e5450a38ea
Pio Molina <vpio@comcast.net>
2018-08-29 16:04:36 -0400

    Changed Styling

--- src/App.js
+++ src/App.js
@@ -9,10 +9,19 @@ import SocialLinks from './components/SocialLinks';
 import Footer from './components/Footer';
 import './App.css';
 
+
+let bgColors = { "Default": "#81b71a",
+                    "Blue": "#00B1E1",
+                    "Cyan": "#37BC9B",
+                    "Green": "#8CC152",
+                    "Red": "#E9573F",
+                    "Yellow": "#F6BB42",
+                  };
+
 class App extends Component {
   render() {
     return (
-      <div>
+      <div style={{backgroundColor: bgColors.Yellow}}>
         <Navbar />
         <Home />
         <Main />
